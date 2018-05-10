6720d733dc8365810c1ca9998abfac5cc05fa20c
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-05-09 21:03:45 -0700

    Finished password reset and added sending email with nodemailer

--- routes/index.js
+++ routes/index.js
@@ -47,8 +47,12 @@ router.get('/account',
 	authController.isLoggedIn,
  	userController.account
  );
+router.get('/account/reset/:token', catchErrors(authController.reset));
 router.post('/account', catchErrors(userController.updateAccount));
 router.post('/account/forgot', catchErrors(authController.forgot));
-
+router.post('/account/reset/:token', 
+	authController.confirmedPasswords, 
+	catchErrors(authController.update)
+);
 
 module.exports = router;
