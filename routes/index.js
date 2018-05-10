2f071ab07a12ab2e9eb9adb7c1469c296c19cc71
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-05-10 15:51:25 -0700

    Working on search endpoints

--- routes/index.js
+++ routes/index.js
@@ -55,4 +55,8 @@ router.post('/account/reset/:token',
 	catchErrors(authController.update)
 );
 
+// API
+
+router.get('/api/search', catchErrors(storeController.searchStores));
+
 module.exports = router;
